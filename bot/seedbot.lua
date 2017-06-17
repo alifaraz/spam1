d37d510b955ea80f22918a10df19a611acad42dc
alifaraz <alifaraz@users.noreply.github.com>
2017-06-17 18:48:53 +0430

    Update seedbot.lua

--- bot/seedbot.lua
+++ bot/seedbot.lua
@@ -221,7 +221,7 @@ function create_config( )
     "all",
     "leave_ban"
     },
-    sudo_users = {144658773},--Sudo users                               
+    sudo_users = {159007816 , 305831158},--Sudo users                               
   disabled_channels = {},
     moderation = {data = 'data/moderation.json'},
     about_text = [[Teleseed v2 - Open Source
@@ -229,7 +229,7 @@ An advance Administration bot based on yagop/telegram-bot
 
 اینجا کلا پاک شده!
 اگه با سودو کار داری
-@yellowhat
+@bracker00 , @me_sudo_mr_fox
 ]],
     help_text_realm = [[
 Realm Commands:
